462efd6d27198aae877b18c31a3dff847b2f2eeb
Christopher Lucas <cwlucas41@orodruin.ds.slu.edu>
2017-03-09 15:54:43 -0600

    added sequence number to Sample and updated references

--- src/main/java/edu/slu/iot/realdaq/Sample.java
+++ src/main/java/edu/slu/iot/realdaq/Sample.java
@@ -2,12 +2,28 @@ package edu.slu.iot.realdaq;
 
 public class Sample implements Comparable<Sample> {
 	
+	private int sequenceNumber;
 	private String deviceID;
 	private String sessionID;
 	private long timestamp;
 	private float value;
 	
-	public Sample(String deviceID, String sessionID, long timestamp, float value) {
+	public static int nextSequenceNumber(int sequenceNumber) {
+		if (0 <= sequenceNumber && sequenceNumber < Integer.MAX_VALUE) {
+			return sequenceNumber + 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public Sample(Sample prevSample, String deviceID, String sessionID, long timestamp, float value) {
+		
+		if (prevSample == null) {
+			sequenceNumber = 0;
+		} else {
+			sequenceNumber = nextSequenceNumber(prevSample.getSequenceNumber());
+		}
+		
 		this.deviceID = deviceID;
 		this.sessionID = sessionID;
 		this.timestamp = timestamp;
@@ -30,11 +46,15 @@ public class Sample implements Comparable<Sample> {
 	@Override
 	public int compareTo(Sample o) {
 		// sorts by increasing timestamp
-		return ((Long) timestamp).compareTo(o.timestamp);
+		return ((Integer) sequenceNumber).compareTo(o.sequenceNumber);
 	}
 
 	@Override
 	public String toString() {
 		return "deviceID: " + deviceID + ", sessionID: " + sessionID + ", timestamp: " + timestamp + " value: " + value;
+	}
+
+	public int getSequenceNumber() {
+		return sequenceNumber;
 	}
 }
