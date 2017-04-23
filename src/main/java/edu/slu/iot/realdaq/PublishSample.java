dddf73d44b4c7e763c9d52c0f389601941e216c1
Demo User <mmasito123@yahoo.com>
2017-04-23 23:35:17 +0000

    added i2c functionality

--- src/main/java/edu/slu/iot/realdaq/PublishSample.java
+++ src/main/java/edu/slu/iot/realdaq/PublishSample.java
@@ -7,10 +7,40 @@ import edu.slu.iot.IoTClient;
 
 public class PublishSample {
 	
+  private class Gain{
+    
+    public float gain;
+    public int hexcodeR1;
+    public int hexcodeR2;
+
+    Gain(float gain,int hexcodeR1,int hexcodeR2){
+      this.gain = gain;
+      this.hexcodeR1 = hexcodeR1;
+      this.hexcodeR2 = hexcodeR2;
+    
+    }
+  }
+
+  private class Frequency{
+    public float sampleFreq;
+    public int hexcode;
+
+    Frequency(float sampleFreq, int hexcode){
+      this.sampleFreq = sampleFreq;
+      this.hexcode = hexcode;
+    }
+  }
+
+
 	public static void main(String args[]) throws InterruptedException, AWSIotException, AWSIotTimeoutException {
-   
-		IoTClient client = new IoTClient("/home/debian/ECE-Capstone-Demo/Certificate1/conf.txt");
-    	
+    
+    //init 
+    Runtime.getRuntime.exec("python adjustableResistors.py");		
+    IoTClient client = new IoTClient("/home/debian/ECE-Capstone-Demo/Certificate1/conf.txt");
+    
+
+    
+
 		client.publish(new DaqPublisher(client, "RealDaq", AWSIotQos.QOS0, "DAQ"));
 
     }
